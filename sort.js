25a959a18876e57410c4347273e1bede6f0ac8e2
Queen <queennefertitishabazz@gmail.com>
2020-02-27 12:37:26 -0800

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,38 @@
+// bubble sort looping through an array to find out whether adjacent values need swapping, until there are no more values that need swapping:
+
+function swap(array, i, j) {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+};
+
+function bubbleSort(array) {
+    let swaps = 0;
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1);
+            swaps++;
+        }
+    }
+
+    if (swaps > 0) {
+        return bubbleSort(array);
+    }
+    return array;
+};
+
+//Merge sort breaks the array down into continually smaller chunks, then merges them back together in the correct order:
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return merge(left, right, array);
+};
